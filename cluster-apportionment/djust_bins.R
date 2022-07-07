cb331306c41c174692afa47cb1aaa6e843672b98
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-07-07 08:16:37 -0300

    up to publication

---
+++ cluster-apportionment/djust_bins.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+
+import_dataset <- function(data_path) {
+  #` Import colocation dataset into a tibble
+  #`
+
+}
+
+interpolation <- function(y, x, x_out, method, ...) {
+  #` Interpolate dataset and return new sampling
+  #`
+
+
+  # Apply stats::approx
+}
+
+main <- function() {
+
+}
+
+if (!interactive()) {
+  main()
+}
